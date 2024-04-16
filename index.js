f797e8582556f6eec6a1fe706f7ed689b817c47b
Miguel Soto <miguelsr.1084@gmail.com>
2024-04-15 22:37:53 -0500

    init proyect in native

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-import App from './App';
+import {App} from './App';
 import {name as appName} from './app.json';
+import {PaperProvider} from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+import IonIcon from 'react-native-vector-icons/Ionicons';
+
+const Main = () => {
+  return (
+    <PaperProvider settings={{icon: props => <IonIcon {...props} />}}>
+      <App />
+    </PaperProvider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Main);
+
+export default Main;
---
+++ src/screens/CustomerEdit.tsx
@@ -0,0 +1,7 @@
+import {View} from 'react-native';
+
+const CustomerEdit = () => {
+  return <View></View>;
+};
+
+export default CustomerEdit;
